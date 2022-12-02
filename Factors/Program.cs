d9ca698d65f08d9bc0b77228ae2a64a9219b797d
ArtemLevin <python_study_2022@mail.ru>
2022-12-03 01:39:36 +0300

    Разложение на множители готово

---
+++ Factors/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Write("Enter the number: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(" ");
+int i;
+Console.Write($"{N} = 1");
+while (N != 1)
+ {
+    i=2;
+    while (N%i!=0)
+    {
+      i++;
+    }
+    Console.Write("*"+i);
+    N = N/i;
+ }
