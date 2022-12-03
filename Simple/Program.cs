fa4c3f6328934aabcdbe4e5e451e97d172bad8df
ArtemLevin <python_study_2022@mail.ru>
2022-12-03 16:57:51 +0300

    Добавлено разложение числа на множители, определение типа числа, нахождение НОД для трех чисел

---
+++ Simple/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Enter the number: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(" ");
+int i;
+Console.Write($"{N} = 1");
+int count = 0;
+while (N != 1)
+ {
+    i=2;
+    while (N%i!=0)
+    {
+      i++;
+    }
+    Console.Write("*"+i);
+    N = N/i;
+    count ++;
+ }
+
+ if (count == 1)
+ {
+    Console.WriteLine(" ");
+    Console.Write("Введено простое число");
+ }
+ else
+ {
+    Console.WriteLine(" ");
+    Console.Write("Введено составное число");
+ }
