646bfae1c0a3b4aa815ef2dd47fc14e066504eaa
ArtemLevin <python_study_2022@mail.ru>
2022-12-04 16:09:08 +0300

    поиск НОК

---
+++ LCM/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter the first number: ");
+int x = int.Parse(Console.ReadLine())!;
+
+Console.Write("Enter the second number: ");
+int y = int.Parse(Console.ReadLine()!);
+int i = 1;
+
+while (i <=x*y)
+
+{
+    if (i%x == 0 && i%y ==0)
+    {
+        Console.Write ($"LCM is {i} ");
+        break;
+    }
+    i++;
+}
+
+
