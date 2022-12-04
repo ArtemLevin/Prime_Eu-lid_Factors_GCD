f6b3894eab8524a392dec94b8ec0f8c8f5ed76f0
ArtemLevin <python_study_2022@mail.ru>
2022-12-04 15:17:12 +0300

    дискриминант

---
+++ Square/Program.cs
@@ -0,0 +1,26 @@
+﻿int a, b, c, n;
+a = -10;
+while (a<11)
+{
+    b = -10000;
+    while (b<11)
+    {
+        c = -10;
+        while (c<11)
+        {
+            n = -10;
+            while (n<11)
+            {
+                if ((b*b - 4*a*c) == (b + n)*(b + n) && c!=0 && a!=0 && b!=0 && (b*b - 4*a*c) > 10000 )
+                {
+                    Console.WriteLine ($" a = {a}, b = {b}, c = {c}, n = {n}");
+                }
+                n++;
+            }
+            c++;
+        }
+        b++;
+        
+    }
+    a++;
+}
