29f42846fabd675eaa8e89e88862c020b8bd3158
ArtemLevin <python_study_2022@mail.ru>
2022-12-04 15:55:22 +0300

    форматирование

---
+++ HelpFile/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write(" Введите число N = ");
+double N = Convert.ToDouble(Console.ReadLine());
+string output =string.Format($" Результат деления:  + {N/3:F}");
+Console.Write(output);
+
+string sq =string.Format($" Результат извлечения корня:  + {Math.Sqrt(N):F}");
+Console.Write(sq);
+
+
