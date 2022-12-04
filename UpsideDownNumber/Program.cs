b5e1316af1f0034bb397d0f6ee170a52832d96a9
ArtemLevin <python_study_2022@mail.ru>
2022-12-04 16:29:23 +0300

    Поиска обратного числа

---
+++ UpsideDownNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter the first number: ");
+int x = int.Parse(Console.ReadLine())!;
+
+int ost= 0;
+int newnum = 0;
+int newost;
+
+while (x!=0)
+{
+    newost = x%10;
+    newnum = ost*10 + newost;
+    ost = newnum;
+    x/=10;
+}
+
+Console.Write("Число наоборот: " + newnum);
