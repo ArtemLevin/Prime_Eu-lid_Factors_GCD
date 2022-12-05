2e96704290342a4bf60e3bb46c0ee4ebbc88ed74
ArtemLevin <python_study_2022@mail.ru>
2022-12-06 00:31:09 +0300

    Decimal to binary

---
+++ decimalToBinary/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int n =  int.Parse(Console.ReadLine()!);
+int ost; 
+string newn;
+while (n!=0)
+{
+    ost = n%2;
+    n = n/2;
+    newn = Convert.ToString(ost);
+    Console.Write(newn);
+}
