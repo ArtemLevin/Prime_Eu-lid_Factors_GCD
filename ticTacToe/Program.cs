6fceedcf0d43463cc7f43089451d9ac5b9f6b9d4
ArtemLevin <python_study_2022@mail.ru>
2022-12-07 22:46:56 +0300

    TicTacToe first step

---
+++ ticTacToe/Program.cs
@@ -0,0 +1,20 @@
+﻿int i, j, k;
+
+for (k=1; k<=3; k++)
+{
+    for (i=0; i<=18; i++)
+    {
+        Console.Write("-");
+    }
+    Console.WriteLine(" ");
+    
+    for (j=0; j<=6; j++);
+    {
+        Console.WriteLine("|     |     |     |");
+    }
+}
+ for (i=0; i<=18; i++)
+    {
+        Console.Write("-");
+    }
+
