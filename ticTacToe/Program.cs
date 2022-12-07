391160c173399a188f6dc6f8e7d7972e535f14df
ArtemLevin <python_study_2022@mail.ru>
2022-12-08 00:06:33 +0300

    TTT

--- ticTacToe/Program.cs
+++ ticTacToe/Program.cs
@@ -1,6 +1,6 @@
 ﻿
 Console.SetCursorPosition(0, 5);
-int i, j, k, Stolb, Stroka, p, R;
+int i, j, k, Column, Line, p, R;
 string Point;
 
 for (k = 1; k <= 3; k++)
@@ -10,59 +10,55 @@ for (k = 1; k <= 3; k++)
         Console.Write("-");
     }
     Console.WriteLine(" ");
-
-    for (j = 0; j <= 6; j++) ;
-    {
-        Console.WriteLine("|     |     |     |");
-    }
+    Console.WriteLine("|     |     |     |");
 }
 for (i = 0; i <= 18; i++)
 {
     Console.Write("-");
 }
-void location(int Stolb, int Stroka, string Point)
+void location(int Column, int Line, string Point)
 
 {
-    if (Stolb == 1 && Stroka == 1)
+    if (Column == 1 && Line == 1)
     {
         Console.SetCursorPosition(3, 6);
     }
-    if (Stolb == 1 && Stroka == 2)
+    if (Column == 1 && Line == 2)
     {
         Console.SetCursorPosition(3, 8);
     }
-    if (Stolb == 1 && Stroka == 3)
+    if (Column == 1 && Line == 3)
     {
         Console.SetCursorPosition(3, 10);
     }
-    if (Stolb == 2 && Stroka == 1)
+    if (Column == 2 && Line == 1)
     {
         Console.SetCursorPosition(9, 6);
     }
-    if (Stolb == 2 && Stroka == 2)
+    if (Column == 2 && Line == 2)
     {
         Console.SetCursorPosition(9, 8);
     }
-    if (Stolb == 2 && Stroka == 3)
+    if (Column == 2 && Line == 3)
     {
         Console.SetCursorPosition(9, 10);
     }
-    if (Stolb == 3 && Stroka == 1)
+    if (Column == 3 && Line == 1)
     {
         Console.SetCursorPosition(15, 6);
     }
-    if (Stolb == 3 && Stroka == 2)
+    if (Column == 3 && Line == 2)
     {
         Console.SetCursorPosition(15, 8);
     }
-    if (Stolb == 3 && Stroka == 3)
+    if (Column == 3 && Line == 3)
     {
         Console.SetCursorPosition(15, 10);
     }
     Console.Write($"{Point}");
 }
 
-for (p = 1; p <= 10; p++)
+for (p = 1; p <= 9; p++)
 {
     if (p % 2 != 0)
     {
@@ -77,11 +73,11 @@ for (p = 1; p <= 10; p++)
 
     Console.WriteLine(" ");
     Console.Write($" Игрок {R} введите номер столбца: ");
-    Stolb = int.Parse(Console.ReadLine()!);
+    Column = int.Parse(Console.ReadLine()!);
     Console.Write($" Игрок {R} введите номер строки: ");
-    Stroka = int.Parse(Console.ReadLine()!);
-    location(Stolb, Stroka, Point);
+    Line = int.Parse(Console.ReadLine()!);
+    location(Column, Line, Point);
     Console.SetCursorPosition(0, 11);
-
-
 }
+
+Console.SetCursorPosition(0, 14);
