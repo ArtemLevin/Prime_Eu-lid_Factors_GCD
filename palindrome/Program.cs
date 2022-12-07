1a5119b3fe9fd44e897ee87a2d01277bc07c6131
ArtemLevin <python_study_2022@mail.ru>
2022-12-07 21:36:21 +0300

    rray orted

---
+++ palindrome/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int rev = 0;
+int rem;
+int orin;
+Console.WriteLine("Введите число: ");
+int n =  int.Parse(Console.ReadLine()!);
+orin = n;
+
+while (n!=0)
+{
+    rem = n%10; 
+    rev = rev*10 + rem;
+    n=n/10;
+}
+
+if (orin == rev)
+{
+    Console.WriteLine("палиндром");
+}
+else
+{
+    Console.WriteLine("Не явялется палиндромом");
+}
